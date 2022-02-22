6b255cf6a3e7dadddf708b667a46452ec9646f70
Lalodev <laloing87@gmail.com>
2022-02-22 12:48:11 -0600

    DOM, ajax, spa

---
+++ ajax-ejercicios/assets/stripe-keys.js
@@ -0,0 +1,6 @@
+export default {
+    public: "pk_test_51KSDNMAgoreP1nGwAxUmkPxVMpOgeYwrdFNtjGrCaB02x03jCbC6jW5FqdqPoEcVMxu9sVy8QitXH2OvAwSCbmbD00q8niczoT",
+    secret: "sk_test_51KSDNMAgoreP1nGwhr5nTP9LggCfkekaRbP5r6SZ7BHuGikTT3HuCOMbWDoIONFSl4QVwpvOqJdHZq9g0JXH9ywv00CfKYVQcp"
+}
+
+
